a26d8fa82d010bd6c0147e1ed8d7dd15ce49ef5a
nmessa <nmessa@sau16.org>
2020-08-29 18:14:51 -0400

    Add files via upload

---
+++ Lab Exercise 9.13.2020/seconds_old.py
@@ -0,0 +1,18 @@
+##Author: 
+##Lab Exercise 9.13.2020 Problem 4
+
+#Enter your age as an integer and store it in age
+#Add code here
+
+
+#Calculate your age in seconds
+#Add code here
+
+
+#Output the result
+#Add code here
+
+
+##Sample Output
+##Enter your age: 66
+##2081376000
---
+++ Lab Exercise 9.13.2020/timeToStar.py
@@ -0,0 +1,26 @@
+##Author: 
+##Lab Exercise 9.13.2020 Problem 5
+
+#Define variables c, lightYears, secondsPerMinute, minutesPerHour, daysPerYear and assign values
+#Add code here
+
+
+#Calculate the time traveled and store in timeTraveled
+#Add code here
+
+
+#Output the time traveled
+#Add code here
+
+
+#Calculate the number of miles traveled and store in distance
+#Add code here
+
+
+#Output the distance traveled
+#Add code here
+
+
+##Output
+##133081920.0 seconds
+##24753237120000.0 miles
