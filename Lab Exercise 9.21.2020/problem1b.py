bd4a32ca7b5a77794de4fc55e9364f1036fef110
nmessa <nmessa@sau16.org>
2020-09-20 12:37:21 -0400

    Add files via upload

---
+++ Lab Exercise 9.21.2020/problem1b.py
@@ -0,0 +1,22 @@
+##Lab Exercise 9.21.2020 Problem 1b
+##Author: 
+##This program will calculate the area and perimeter of a
+##rectangular room
+
+#initialize values of width and length
+#Add code here
+
+
+#make calculation of area and store in variable area
+#Add code here
+
+#make calculation of perimeter and store in variable perimeter
+#Add code here
+
+#print results
+print ('Area =', area)
+print ('Perimeter =', perimeter)
+
+## Output
+## Area = 208.75
+## Perimeter = 58.4
